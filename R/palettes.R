f8d316eadc99b4ef4de09e3fb0097446c309ccae
Michael Skinnider <michaelskinnider@gmail.com>
2019-06-27 17:32:58 -0700

    start digitizing palettes

---
+++ R/palettes.R
@@ -0,0 +1,76 @@
+ivy_league = c("#B6AA95", 
+               "#585748",
+               "#443731",
+               "#B92636")
+snug_harbor = c("#094e81",
+                "#e52841",
+                "#9f9db5",
+                "#0a70af")
+
+fresh_air = c(
+  "morning mist" = "#CCE0DC",
+  "lightest sky" = "#E6EDE4",
+  "white sand" = "#DCD7D4",
+  "acacia" = "#DDD161"
+) %>% unname()
+
+jewelry_chest = c(
+  "african sapphire" = "#201747",
+  "serpentine" = "#173F35",
+  "apple jade" = "#008755",
+  "black onyx" = "#2D2926"
+) %>% unname()
+florida_citrus = c(
+  "Blazing Yellow" = "#FEE715",
+  "Bright Marigold" = "#FF8D00",
+  "Sheer Pink" = "#F4EAE0",
+  "Classic Green" = "#32B141"
+) %>% unname()
+party_dress = c(
+  "Pink Crinoline" = "#cf96b0",
+  "Cool Celadon" = "#D1E0D7",
+  "Soft Jade" = "#7da18f",
+  "Cherry Blossom" = "#F5DADF"
+) %>% unname()
+
+sunset_beach = c(
+  "Hot Pink" = "#E95985",
+  "Hawaiian Ocean" = "#0095C6",
+  "Fiesta" = "#DC443A",
+  "Purple Wine" = "#8F3975"
+) %>% unname()
+
+island_cruise = c(
+  "Scuba Blue" = "#00B2CA",
+  "Baja Blue" = "#5265B0",
+  "Bone White" = "#D8D4C2",
+  "Macaw Green" = "#9DB73D"
+) %>% unname()
+
+weekend_getaway = c(
+  "Deep Lake" = "#00646B",
+  "Olive Night" = "#565447",
+  "Golden Rod" = "#E9A916",
+  "Moth" = "#D4D0B1"
+) %>% unname()
+
+firecracker = c(
+  "French Blue" = "#1278B9",
+  "Bright White" = "#F1F2F1",
+  "Atmosphere" = "#A39890",
+  "Grenadine" = "#DA413D"
+) %>% unname()
+
+underwater_world = c(
+  "Light Grey" = "#DBD9CE",
+  "Cayenne" = "#DA4A52",
+  "Jade Cream" = "#61BD93",
+  "Tender Shoots" = "#B9D146"
+) %>% unname()
+
+cottage_garden = c(
+  "Young Wheat" = "#E2E3A7",
+  "Garden Green" = "#485C37",
+  "Nimbus Cloud" = "#DFDFE3",
+  "Skyway" = "#ACC0D9"
+) %>% unname()
